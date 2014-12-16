2f747f15fa08844623b4559da0cd457d5329e226
Matthew McClure <matt@mmcc.io>
2014-12-15 17:42:17 -0800

    fixed bugs around successful vs complete states, added late route

--- examples/suite-example.js
+++ examples/suite-example.js
@@ -2,27 +2,14 @@ var ChainSmoker = require('chain-smoker');
 
 var Suite = {};
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 Suite.tests = [
-  {
-    address: 'http://www.example.com/accounts/'+ getRandomInt(100, 999) +'/subscriptions',
-    method: 'POST',
-    body: {
-      endpoint: this.callbackAddress,
-      events: ['herp', 'derp']
-    },
-    callback: true,
-    assertions: {
-      statusCode: 201
-    }
-  },
   {
     address: 'http://example.com',
     assertions: {
-      statusCode: 200
+      statusCode: 200,
+      html_header: function(response) {
+        return response.headers['content-type'] === 'text/html';
+      }
     }
   }
 ];
